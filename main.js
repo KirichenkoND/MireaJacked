fa797b2c6529d12dd496f4c0b744ad7956da4d47
Nikita Kirichenko <nikita.nikita.0413@yandex.ru>
2024-05-25 11:47:37 +0300

    Fix freeze timers and allow copy n paste

--- main.js
+++ main.js
@@ -2,26 +2,20 @@ let s = document.createElement('script');
 s.text = `
   console.log('Injected MireaJacked');
 
-  // Находим элемент по его ID
   const quizTimer = document.getElementById('quiz-timer');
 
-  // Изменяем цвет фона элемента
   if (quizTimer) {
-    quizTimer.style.backgroundColor = '#f0f0f0'; // светло-серый цвет
+    quizTimer.style.backgroundColor = '#f0f0f0'; 
   }
 
-  // Переопределите функцию window.open, чтобы открывать все в новых вкладках, но правильно
-  const originalOpen = window.open; // Сохраняем оригинальную функцию
+  const originalOpen = window.open; 
   window.open = function(url, windowName, windowFeatures) {
-    // Если URL не задан или это 'about:blank', используем оригинальную функцию
     if (!url || url === 'about:blank') {
       return originalOpen.apply(this, arguments);
     }
-    // Иначе открываем URL в новой вкладке
     return originalOpen(url, '_blank');
   };
 
-  // Перехватывать и обрабатывать события нажатия клавиш для Ctrl+C и Ctrl+V
   document.addEventListener('keydown', function(event) {
     if (event.ctrlKey && (event.keyCode === 67 || event.keyCode === 86)) { // Ctrl+C or Ctrl+V
       if (event.keyCode === 67) { // Ctrl+C
@@ -32,35 +26,33 @@ s.text = `
         });
       } else if (event.keyCode === 86) { // Ctrl+V
         navigator.clipboard.readText().then(text => {
-          document.activeElement.value += text; // Assumes the active element can accept text input
+          document.activeElement.value += text; 
           console.log('Pasting from clipboard was successful!');
         }).catch(err => {
           console.error('Failed to read clipboard contents: ', err);
         });
       }
-      event.preventDefault(); // Prevent other handlers from executing
-      event.stopPropagation(); // Stop the event from bubbling up
+      event.preventDefault(); 
+      event.stopPropagation(); 
     }
-  }, true); // Use capture to ensure this runs before other listeners
+  }, true); 
 
-  // Override security settings preventing copying, pasting, etc.
   M.mod_quiz.secure_window = {
     init: function(Y) {
       console.log('Secure window functions have been completely overridden.');
-      Y.delegate = function(type, fn, el, filter) { /* ignore all events */ };
-      Y.on = function(type, fn, el, key) { /* ignore all events */ };
+      Y.delegate = function(type, fn, el, filter) { /* 12345 */ };
+      Y.on = function(type, fn, el, key) { /* 12345 */ };
     },
     prevent_selection: function(e) { return false; },
     prevent: function(e) { return false; },
     prevent_mouse: function(e) { return false; },
     is_content_editable: function(n) { return true; },
-    close: function(url, delay) { /* do nothing */ },
-    init_close_button: function(Y, url) { /* do nothing */ }
+    close: function(url, delay) { /* 12345 */ },
+    init_close_button: function(Y, url) { /* 12345 */ }
   };
 
-  // Intercept and handle copy and paste events for context menu
 document.addEventListener('copy', function(event) {
-  event.preventDefault(); // Prevent default copy behavior
+  event.preventDefault(); 
   navigator.clipboard.writeText(window.getSelection().toString()).then(function() {
     console.log('Copying to clipboard was successful!');
   }, function(err) {
@@ -69,9 +61,9 @@ document.addEventListener('copy', function(event) {
 });
 
 document.addEventListener('paste', function(event) {
-  event.preventDefault(); // Prevent default paste behavior
+  event.preventDefault();
   navigator.clipboard.readText().then(text => {
-    document.activeElement.value += text; // Assumes the active element can accept text input
+    document.activeElement.value += text;
     console.log('Pasting from clipboard was successful!');
   }).catch(err => {
     console.error('Failed to read clipboard contents: ', err);
@@ -79,15 +71,13 @@ document.addEventListener('paste', function(event) {
 });
 
 
-  // Allow context menu
+ 
   document.addEventListener('contextmenu', function(event) {
-    event.stopPropagation(); // Stop the event from bubbling up
-  }, true); // Use capture to ensure this runs before other listeners
+    event.stopPropagation(); 
+  }, true); 
 
-  // Replace the global alert function to block alerts
   window.alert = function() { console.log('Blocked an alert attempt.'); };
 
-  // Replace the M object on the window with our overridden settings
   window.M = M;
 `;
 document.documentElement.appendChild(s);
